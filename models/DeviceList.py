be07ed07e25daa3d12cab5c265de14b714af9e72
vchapple17 <vchapple17@gmail.com>
2018-03-11 16:06:36 -0500

    Remove excess file

--- models/DeviceList.py
+++
@@ -1,8 +0,0 @@
-from google.appengine.ext import ndb
-from datetime import datetime
-import json
-
-class DeviceList(ndb.Model):
-    device_key = ndb.StringProperty()
-    start_datetime = ndb.DateProperty()
-    end_datetime = ndb.DateProperty()
